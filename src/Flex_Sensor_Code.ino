cbfce1d012b29079d443094b5a631a2f79ed7fc1
yiern <chong.yiern@gmail.com>
2020-07-22 11:27:05 +0800

    renamed variables for easier understanding

--- src/Flex_Sensor_Code.ino
+++ src/Flex_Sensor_Code.ino
@@ -9,11 +9,11 @@ voltage at A0 should decrease.
 #include <Arduino.h>
 SoftwareSerial BT(2, 3); // rx,tx
 
-const int FLEX_PIN0 = A0; //Pin connected to voltage divider output (1418)
-const int FLEX_PIN1 = A1; //Pin connected to volatge divider output (0918)
-const int FLEX_PIN4 = A4;
-const int FLEX_PIN5 = A3;
-const int FLEX_PIN6 = A2;
+const int THUMB = A0; //thumb
+const int INDEX = A1; //index
+const int PINKY = A4; //pinky
+const int RING = A3; //ring
+const int MIDDLE = A2; //middle
 
 //Measure the voltage at 5V and the actual resistance of your
 //10k resistor, and enter them below:
@@ -39,11 +39,11 @@ const float BEND_RESISTANCE6 = 22000.0; //resistance at 90 deg for PIN6
 
 void setup() 
 {
-  pinMode(FLEX_PIN0, INPUT);
-  pinMode(FLEX_PIN1, INPUT);
-  pinMode(FLEX_PIN4, INPUT);
-  pinMode(FLEX_PIN5, INPUT);
-  pinMode(FLEX_PIN6, INPUT);
+  pinMode(THUMB, INPUT);
+  pinMode(INDEX, INPUT);
+  pinMode(PINKY, INPUT);
+  pinMode(RING, INPUT);
+  pinMode(MIDDLE, INPUT);
   Serial.begin(9600);
   BT.begin(9600);
   
@@ -52,21 +52,21 @@ void setup()
 void loop() 
 {
   //Read the ADC, and calculate voltage and resistance from it for PIN0
-  int flexADC0 = analogRead(FLEX_PIN0);
+  int flexADC0 = analogRead (THUMB);
   float flexV0 = flexADC0 * VCC / 1023.0;
   float flexR0 = R_DIV0 * (VCC / flexV0 - 1.0);
 
 
   //Use the calculated resistance to estimate the sensor's
   //bend angle:
-  float angle0 = map(flexR0, STRAIGHT_RESISTANCE0, BEND_RESISTANCE0, 0, 90.0);
+  float Thumb_Angle = map(flexR0, STRAIGHT_RESISTANCE0, BEND_RESISTANCE0, 0, 90.0);
 
-  //Serial.println("index " + String(angle0));
+  //Serial.println("index " + String(Thumb_Angle));
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
   //Read the ADC, and calculate voltage and resistance from it for PIN1
-  int flexADC1 = analogRead(FLEX_PIN1);
+  int flexADC1 = analogRead(INDEX);
   float flexV1 = flexADC1 * VCC / 1023.0;
   float flexR1 = R_DIV1 * (VCC / flexV1 - 1.0);
   
@@ -74,14 +74,14 @@ void loop()
 
   //Use the calculated resistance to estimate the sensor's
   //bend angle:
-  float angle1 = map(flexR1, STRAIGHT_RESISTANCE1, BEND_RESISTANCE1, 0, 90.0);
+  float Index_Angle = map(flexR1, STRAIGHT_RESISTANCE1, BEND_RESISTANCE1, 0, 90.0);
 
-   //Serial.println("middle " + String(angle1));
+   //Serial.println("middle " + String(Index_Angle));
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
    //Read the ADC, and calculate voltage and resistance from it for PIN4
-  int flexADC4 = analogRead(FLEX_PIN4);
+  int flexADC4 = analogRead(PINKY);
   float flexV4 = flexADC4 * VCC / 1023.0;
   float flexR4 = R_DIV0 * (VCC / flexV4 - 1.0);
 
@@ -89,14 +89,14 @@ void loop()
 
   //Use the calculated resistance to estimate the sensor's
   //bend angle:
-  float angle4 = map(flexR4, STRAIGHT_RESISTANCE4, BEND_RESISTANCE4, 0, 90.0);
+  float Pinky_Angle = map(flexR4, STRAIGHT_RESISTANCE4, BEND_RESISTANCE4, 0, 90.0);
 
-  //Serial.println("ring " + String(angle4));
+  //Serial.println("ring " + String(Pinky_Angle));
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   //Read the ADC, and calculate voltage and resistance from it for PIN5
-  int flexADC5 = analogRead(FLEX_PIN5);
+  int flexADC5 = analogRead(RING);
   float flexV5 = flexADC5 * VCC / 1023.0;
   float flexR5 = R_DIV0 * (VCC / flexV5 - 1.0);
   
@@ -104,25 +104,25 @@ void loop()
 
   //Use the calculated resistance to estimate the sensor's
   //bend angle:
-  float angle5 = map(flexR5, STRAIGHT_RESISTANCE5, BEND_RESISTANCE5, 0, 90.0);
+  float Ring_Angle = map(flexR5, STRAIGHT_RESISTANCE5, BEND_RESISTANCE5, 0, 90.0);
 
  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  int flexADC6 = analogRead(FLEX_PIN6);
-  float flexV6 = flexADC5 * VCC / 1023.0;
+  int flexADC6 = analogRead(MIDDLE);
+  float flexV6 = flexADC6 * VCC / 1023.0;
   float flexR6 = R_DIV0 * (VCC / flexV6 - 1.0);
   
 
 
   //Use the calculated resistance to estimate the sensor's
   //bend angle:
-  float angle6 = map(flexR6, STRAIGHT_RESISTANCE6, BEND_RESISTANCE6, 0, 90.0);
+  float Middle_Angle = map(flexR6, STRAIGHT_RESISTANCE6, BEND_RESISTANCE6, 0, 90.0);
 
- Serial.println(String(angle0) + ","  + String(angle1) +  "," + String(angle4)+ "," + String(angle5)+ "," + String(angle6));
+Serial.println(String(Thumb_Angle) + ","  + String(Index_Angle) +  "," + String(Pinky_Angle)+ "," + String(Ring_Angle)+ "," + String(Middle_Angle));
   
-  BT.println(String(angle0) + "," + String(angle6) + "," + String(angle1) + "," + String("0")+ "," + String(angle4)+ "," + String(angle5));
+BT.println(String(Thumb_Angle) + "," + String(Middle_Angle) + "," + String(Index_Angle) + "," + String("0")+ "," + String(Pinky_Angle)+ "," + String(Ring_Angle));
 
   
   
-  delay(500); //delay 5 sec before sending sensor 2 data
+  delay(500); //delay 1/2 sec before sending sensor 2 data
 }
