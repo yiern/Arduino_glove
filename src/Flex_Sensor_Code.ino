5bdfd871908a510abd83f9a9ffa9250dd5e98630
yiern <chong.yiern@gmail.com>
2020-07-24 11:13:17 +0800

    Update Flex_Sensor_Code.ino

--- src/Flex_Sensor_Code.ino
+++ src/Flex_Sensor_Code.ino
@@ -24,17 +24,17 @@ const float R_DIV1 = 9800.0; //Measured resistance of 10k resistor for PIN1
 
 //Upload the code, then try to adjust these values to more
 //accurately calculate bend degree.
-const float STRAIGHT_RESISTANCE0 = 11350.0 ; //resistance when straight for PIN0
-const float BEND_RESISTANCE0 = 39400.0; //resistance at 90 deg for PIN0
-const float STRAIGHT_RESISTANCE1 = 18000.0 ; //resistance when straight for PIN1
-const float BEND_RESISTANCE1 = 52400.0; //resistance at 90 deg for PIN1
-const float STRAIGHT_RESISTANCE4 = 9800.0 ; //resistance when straight for PIN4
-const float BEND_RESISTANCE4 = 19400.0; //resistance at 90 deg for PIN4
-const float STRAIGHT_RESISTANCE5 = 94000.0 ; //resistance when straight for PIN4
-const float BEND_RESISTANCE5 = 145000.0; //resistance at 90 deg for PIN4
-const float STRAIGHT_RESISTANCE6 = 11170.0 ; //resistance when straight for PIN6
-const float BEND_RESISTANCE6 = 22000.0; //resistance at 90 deg for PIN6
-
+const float Thumb_StraightResistance      = 94000.0; //resistance when straight for PIN0
+const float Thumb_BendResistance          = 113000.0; //resistance at 90 deg for PIN0
+const float Index_StraightResistance      = 13590.0; //resistance when straight for PIN1
+const float Index_BendResistance          = 32000.0; //resistance at 90 deg for PIN1
+const float Pinky_StraightResistance      = 12000.0 ; //resistance when straight for PIN4
+const float Pinky_BendResistance          = 30000.0; //resistance at 90 deg for PIN4
+const float Ring_StraightResistance       = 23500.0 ; //resistance when straight for PIN4
+const float Ring_BendResistance           = 73500.0; //resistance at 90 deg for PIN4
+const float Middle_StraightResistance     = 11070.0 ; //resistance when straight for PIN6
+const float Middle_BendResistance         = 23000.0; //resistance at 90 deg for PIN6
+ 
 
 
 void setup() 
@@ -59,7 +59,7 @@ void loop()
 
   //Use the calculated resistance to estimate the sensor's
   //bend angle:
-  float Thumb_Angle = map(flexR0, STRAIGHT_RESISTANCE0, BEND_RESISTANCE0, 0, 90.0);
+  float Thumb_Angle = map(flexR0, Thumb_StraightResistance, Thumb_BendResistance, 0, 90.0);
 
   //Serial.println("index " + String(Thumb_Angle));
 
@@ -74,7 +74,7 @@ void loop()
 
   //Use the calculated resistance to estimate the sensor's
   //bend angle:
-  float Index_Angle = map(flexR1, STRAIGHT_RESISTANCE1, BEND_RESISTANCE1, 0, 90.0);
+  float Index_Angle = map(flexR1, Index_StraightResistance, Index_BendResistance, 0, 90.0);
 
    //Serial.println("middle " + String(Index_Angle));
 
@@ -89,7 +89,7 @@ void loop()
 
   //Use the calculated resistance to estimate the sensor's
   //bend angle:
-  float Pinky_Angle = map(flexR4, STRAIGHT_RESISTANCE4, BEND_RESISTANCE4, 0, 90.0);
+  float Pinky_Angle = map(flexR4, Pinky_StraightResistance, Pinky_BendResistance, 0, 90.0);
 
   //Serial.println("ring " + String(Pinky_Angle));
 
@@ -104,7 +104,7 @@ void loop()
 
   //Use the calculated resistance to estimate the sensor's
   //bend angle:
-  float Ring_Angle = map(flexR5, STRAIGHT_RESISTANCE5, BEND_RESISTANCE5, 0, 90.0);
+  float Ring_Angle = map(flexR5, Ring_StraightResistance, Ring_BendResistance, 0, 90.0);
 
  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -116,11 +116,11 @@ void loop()
 
   //Use the calculated resistance to estimate the sensor's
   //bend angle:
-  float Middle_Angle = map(flexR6, STRAIGHT_RESISTANCE6, BEND_RESISTANCE6, 0, 90.0);
+  float Middle_Angle = map(flexR6, Middle_StraightResistance, Middle_BendResistance, 0, 90.0);
 
-Serial.println("Thumb Reading" + String(Thumb_Angle) + ","  + String(Index_Angle) +  "," + String(Pinky_Angle)+ "," + String(Ring_Angle)+ "," + String(Middle_Angle));
+//Serial.println("Thumb Reading: " + String(Thumb_Angle) + ", Index Reading: "  + String(Index_Angle) +  ", Middle Reading: " + String(Middle_Angle)+ ", Ring Reading: " + String(Ring_Angle)+ ", Pinky reading: " + String(Pinky_Angle));
   
-BT.println(String(Thumb_Angle) + "," + String(Middle_Angle) + "," + String(Index_Angle) + "," + String("0")+ "," + String(Pinky_Angle)+ "," + String(Ring_Angle));
+BT.println(String(Thumb_Angle) + "," + String(Index_Angle) + "," + String(Middle_Angle) + "," + String(Ring_Angle)+ "," + String(Pinky_Angle));
 
   
   
